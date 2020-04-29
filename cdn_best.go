44cfb61dee42b4250b8cb53957f1b1f513774f6c
Jinho Yoon <37324479+ek2rlstk@users.noreply.github.com>
2020-04-29 23:09:42 +0900

    support twimg+twvideo (experimental)

--- cdn_best.go
+++ cdn_best.go
@@ -19,7 +19,7 @@ type cdnTestResult struct {
 	TotalTime uint64
 }
 
-func getBestCdn(addrs []net.IP) string {
+func getBestCdnI(addrs []net.IP) string {
 	testResult := make([]cdnTestResult, len(addrs))
 	for index, addr := range addrs {
 		testResult[index].Addr = addr
@@ -39,7 +39,7 @@ func getBestCdn(addrs []net.IP) string {
 	var g sync.WaitGroup
 	g.Add(len(testResult))
 	for index := range testResult {
-		go cdnTest(&g, index, &testResult[index], bar)
+		go cdnTestI(&g, index, &testResult[index], bar)
 	}
 
 	g.Wait()
@@ -68,7 +68,56 @@ func getBestCdn(addrs []net.IP) string {
 	return testResult[0].Addr.String()
 }
 
-func cdnTest(g *sync.WaitGroup, index int, r *cdnTestResult, bar *pb.ProgressBar) {
+func getBestCdnV(addrs []net.IP) string {
+	testResult := make([]cdnTestResult, len(addrs))
+	for index, addr := range addrs {
+		testResult[index].Addr = addr
+	}
+
+	bar := pb.New(len(testResult))
+	bar.SetMaxWidth(80)
+
+	bar.ShowElapsedTime = true
+	bar.ShowFinalTime = false
+	bar.ShowSpeed = false
+	bar.ShowTimeLeft = false
+
+	bar.Set(0)
+	bar.Start()
+
+	var g sync.WaitGroup
+	g.Add(len(testResult))
+	for index := range testResult {
+		go cdnTestV(&g, index, &testResult[index], bar)
+	}
+
+	g.Wait()
+
+	bar.Finish()
+
+	i := 0
+	for i < len(testResult) {
+		if testResult[i].Failed {
+			copy(testResult[i:], testResult[i+1:])
+			testResult = testResult[:len(testResult)-1]
+		} else {
+			i++
+		}
+	}
+
+	sort.Slice(testResult, func(a, b int) bool { return testResult[a].TotalTime < testResult[b].TotalTime })
+
+	for _, r := range testResult {
+		fmt.Printf("%15s / %7.2d ms\n", r.Addr.String(), r.TotalTime)
+	}
+
+	if len(testResult) == 0 {
+		return ""
+	}
+	return testResult[0].Addr.String()
+}
+
+func cdnTestI(g *sync.WaitGroup, index int, r *cdnTestResult, bar *pb.ProgressBar) {
 	defer func() {
 		bar.Increment()
 		g.Done()
@@ -90,6 +139,60 @@ func cdnTest(g *sync.WaitGroup, index int, r *cdnTestResult, bar *pb.ProgressBar
 	buff := make([]byte, httpBufferSize)
 	for i := 0; i < httpCount; i++ {
 		hreq, err := http.NewRequest("GET", twimgTestURI, nil)
+		if err != nil {
+			return
+		}
+		hreq.Close = true
+
+		hres, err := client.Do(hreq)
+		if err != nil {
+			return
+		}
+		defer hres.Body.Close()
+
+		if !strings.HasPrefix(hres.Header.Get("content-type"), "image") {
+			return
+		}
+
+		start := time.Now()
+		for {
+			read, err := hres.Body.Read(buff)
+			if err != nil && err != io.EOF {
+				return
+			}
+			if read == 0 {
+				break
+			}
+		}
+
+		r.TotalTime += uint64(time.Duration(time.Now().Sub(start).Nanoseconds()) / time.Millisecond)
+	}
+
+	r.Failed = false
+}
+
+func cdnTestV(g *sync.WaitGroup, index int, r *cdnTestResult, bar *pb.ProgressBar) {
+	defer func() {
+		bar.Increment()
+		g.Done()
+	}()
+
+	r.Failed = true
+
+	client := http.Client{
+		Timeout: httpTimeout * time.Second,
+		Transport: &http.Transport{
+			Dial: func(network, addr string) (net.Conn, error) {
+				return net.Dial(network, strings.ReplaceAll(addr, twVideoHostName, r.Addr.String()))
+			},
+			IdleConnTimeout:   httpTimeout * time.Second,
+			DisableKeepAlives: true,
+		},
+	}
+
+	buff := make([]byte, httpBufferSize)
+	for i := 0; i < httpCount; i++ {
+		hreq, err := http.NewRequest("GET", twVideoTestURI, nil)
 		if err != nil {
 			return
 		}
--- main.go
+++ main.go
@@ -12,8 +12,10 @@ import (
 )
 
 const (
-	twimgHostName = "video.twimg.com"
-	twimgTestURI  = "https://video.twimg.com/ext_tw_video/1251066844548489216/pu/vid/1280x720/qFITDS_AeTMnKW49.mp4"
+	twimgHostName   = "pbs.twimg.com"
+	twimgTestURI    = "https://pbs.twimg.com/media/CgAc2lSUMAA30oE.jpg:orig"
+	twVideoHostName = "video.twimg.com"
+	twVideoTestURI  = "https://video.twimg.com/ext_tw_video/1251066844548489216/pu/vid/1280x720/qFITDS_AeTMnKW49.mp4"
 
 	pingCount      = 20
 	pingTimeout    = 1000
@@ -23,13 +25,17 @@ const (
 )
 
 var (
-	regPattern = regexp.MustCompile(fmt.Sprintf("^[^\\s\\t#].+[\\s\\t]+%s.*$", strings.ReplaceAll(twimgHostName, ".", "\\.")))
+	regPatternI = regexp.MustCompile(fmt.Sprintf("^[^\\s\\t#].+[\\s\\t]+%s.*$", strings.ReplaceAll(twimgHostName, ".", "\\.")))
+)
+
+var (
+	regPatternV = regexp.MustCompile(fmt.Sprintf("^[^\\s\\t#].+[\\s\\t]+%s.*$", strings.ReplaceAll(twVideoHostName, ".", "\\.")))
 )
 
 type args struct {
 	Help      bool `cli:"h,help"      usage:"도움말을 표시합니다"`
-	Install   bool `cli:"i,install"   usage:"Twvideo dns 패치를 합니다"`
-	Uninstall bool `cli:"u,uninstall" usage:"Twvideo dns 패치를 제거합니다"`
+	Install   bool `cli:"i,install"   usage:"TwMedia dns 패치를 합니다"`
+	Uninstall bool `cli:"u,uninstall" usage:"TwMedia dns 패치를 제거합니다"`
 }
 
 func (args *args) AutoHelp() bool {
@@ -72,17 +78,17 @@ func patch(args *args) {
 	}()
 
 	if !args.Install || !args.Uninstall {
-		println("Twvideo 패치를 확인중입니다")
+		println("TwMedia 패치를 확인중입니다")
 	}
 
 	lines, patched := readAllHosts(fs)
 
 	if args.Install && patched {
-		println("이미 Twvideo dns 패치가 되어있습니다.")
+		println("이미 TwMedia dns 패치가 되어있습니다.")
 		return
 	}
 	if args.Uninstall && !patched {
-		println("Twvideo dns 패치가 되어있지 않습니다.")
+		println("TwMedia dns 패치가 되어있지 않습니다.")
 		return
 	}
 
@@ -92,11 +98,11 @@ func patch(args *args) {
 			lines = lines[:len(lines)-1]
 		}
 	} else {
-		println("Twvideo cdn 서버 정보를 가져오고 있습니다.")
-		hosts := getAddresses()
+		println("TwImage cdn 서버 정보를 가져오고 있습니다.")
+		hosts := getAddressesI()
 
 		println("가장 연결 상태가 좋은 CDN 을 찾고 있습니다")
-		bestCdnAddr = getBestCdn(hosts)
+		bestCdnAddr = getBestCdnI(hosts)
 
 		if bestCdnAddr == "" {
 			panic("CDN 테스트를 실패하였습니다.\n나중에 다시 시도해주세요.")
@@ -106,6 +112,25 @@ func patch(args *args) {
 			lines = append(lines, "")
 		}
 		lines = append(lines, fmt.Sprintf("%s\t%s", bestCdnAddr, twimgHostName))
+		println("Twimg cdn 을 패치하였습니다.")
+		println("최적의 cdn : " + bestCdnAddr)
+
+		println("TwVideo cdn 서버 정보를 가져오고 있습니다.")
+		hosts = getAddressesV()
+
+		println("가장 연결 상태가 좋은 CDN 을 찾고 있습니다")
+		bestCdnAddr = getBestCdnV(hosts)
+
+		if bestCdnAddr == "" {
+			panic("CDN 테스트를 실패하였습니다.\n나중에 다시 시도해주세요.")
+		}
+
+		if len(lines) > 0 && strings.TrimSpace(lines[len(lines)-1]) != "" {
+			lines = append(lines, "")
+		}
+		lines = append(lines, fmt.Sprintf("%s\t%s", bestCdnAddr, twVideoHostName))
+		println("TwVideo cdn 을 패치하였습니다.")
+		println("최적의 cdn : " + bestCdnAddr)
 	}
 
 	fs.Seek(0, 0)
@@ -117,10 +142,8 @@ func patch(args *args) {
 	writer.Flush()
 
 	if patched {
-		println("Twvideo cdn 패치를 제거하였습니다.")
+		println("TwMedia cdn 패치를 제거하였습니다.")
 	} else {
-		println("Twvideo cdn 을 패치하였습니다.")
-		println("최적의 cdn : " + bestCdnAddr)
 	}
 
 }
@@ -148,7 +171,7 @@ func readAllHosts(fs io.Reader) (lines []string, patched bool) {
 			}
 		}
 
-		if regPattern.MatchString(lineString) {
+		if regPatternI.MatchString(lineString) {
 			befMatched = true
 			patched = true
 		} else {
